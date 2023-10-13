247a86d8a2106138bd3336d68b485822844fce76
Adibrvl <adibrivaldi292@gmail.com>
2023-10-13 16:10:14 +0700

    menambahkan index.html,about.html,contact.html git commit -m menambahkan index.html,about.html,contact.html

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Adib Rivaldi
+        Saya Tinggal Di Daerah Cibitung
+        Olahraga Yang Saya Sukai Berenang";
+
+    }
+}
